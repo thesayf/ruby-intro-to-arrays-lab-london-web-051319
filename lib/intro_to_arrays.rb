cb05670908cefa2ad64519d040eeed61c305ae32
Rori Hinds <r_ori@hotmail.co.uk>
2019-05-05 19:01:26 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,52 @@
+def instantiate_new_array
+  
+  my_new_array = []
+  
+  return my_new_array
+  
+end
+
+def array_with_two_elements
+  
+  my_two_array = ["element1","element2"]
+  
+  return my_two_array
+  
+end
+
+def first_element(array)
+  
+  return array[0]
+
+end
+
+def third_element(array)
+  
+  return array[2]
+  
+end
+
+def last_element(array)
+  
+  return array[-1]
+  
+end
+
+def first_element_with_array_methods(array)
+  
+  return array[0]
+  
+end 
+
+def last_element_with_array_methods(array)
+  
+  return array[-1]
+  
+end
+  
+def length_of_array(array)
+  
+  return array.length
+  
+end   
+  
